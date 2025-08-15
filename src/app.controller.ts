d025ffbdc08784bec258c626067c7f885e31e233
Hugekyung <kiki9510@gmail.com>
2025-08-16 01:50:21 +0900

    feat: kafka 초기 세팅을 위한 controller, service, module 구현, docker-compose.yml 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,17 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/orders')
+  createOrder(@Body() orderData: { userId: string; productId: string }) {
+    return this.appService.createOrder(orderData);
+  }
+
+  @MessagePattern('order.created')
+  handleOrderCreated(@Payload() message: any) {
+    console.log('--- New Order Received ---');
+    console.log('Order Details:', message);
+    console.log('--------------------------');
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createOrder(order: { userId: string; productId: string }) {
+    this.kafkaClient.emit('order.created', JSON.stringify(order));
+    return { message: 'Order created successfully!', order };
   }
 }
